a19c762292f285ebf61c64669647652c1e2e1d37
Eduardo R. D'Avila <erdavila@gmail.com>
2016-12-05 18:28:57 -0200

    ImplicitChaining: Add tracing

--- src/main/scala/ImplicitChaining.scala
+++ src/main/scala/ImplicitChaining.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable
 import scala.language.implicitConversions
 import scala.reflect.ClassTag
 
@@ -8,13 +9,68 @@ object ImplicitChaining extends App {
     def total = x + y + z
   }
 
-  implicit def toA(n: Int) = A(n)
-  implicit def aToB[A1 : ClassTag](a: A1)(implicit f: A1 => A) = B(a.x, 2)
-  implicit def bToC[B1 : ClassTag](b: B1)(implicit f: B1 => B) = C(b.x, b.y, 3)
+  implicit def intToA(n: Int): A = Trace("intToA", n) {
+    A(n)
+  }
+
+  implicit def aToB[T : ClassTag](a: T)(implicit f: T => A): B = Trace("aToB", a, f) {
+    B(a.x, 2)
+  }
+
+  implicit def bToC[T : ClassTag](b: T)(implicit f: T => B): C = Trace("bToC", b, f) {
+    C(b.x, b.y, 3)
+  }
 
   // works
   println(1.total)
   println(A(1).total)
   println(B(1, 2).total)
   println(C(1, 2, 3).total)
+
+  object Trace {
+    private val Indentation = "  "
+    private var level = 0
+    private var lastCalled: Option[String] = None
+    private val enabled = mutable.Stack(true)
+
+    private def indentation = Indentation * level
+
+    def apply[T : ClassTag, U, V](name: String, value: T, f: T => U)(func: => V): V = apply(name, value) {
+      Trace.identifyType[T]()
+      Trace.identifyFunc(f, value)
+      func
+    }
+
+    def apply[T, R](name: String, value: T)(func: => R): R = {
+      println("BEGIN " + name + "(" + value + ": T)")
+      val result = indented {
+        val result = func
+        println("Returning " + result)
+        result
+      }
+      println("END")
+      lastCalled = Some(name)
+      result
+    }
+
+    private def identifyType[T : ClassTag](): Unit =
+      println("T is " + implicitly[ClassTag[T]].runtimeClass.getSimpleName())
+
+    private def identifyFunc[T, R](f: T => R, param: T): Unit = {
+      lastCalled = None
+      enabled.push(false)
+      f(param)
+      enabled.pop()
+      println("f is " + lastCalled.getOrElse("? (probably Predef.$conforms)"))
+    }
+
+    private def indented[R](func: => R): R = {
+      level += 1
+      val result = func
+      level -= 1
+      result
+    }
+
+    private def println(msg: String): Unit = if (enabled.top) Predef.println(indentation + msg)
+  }
 }
